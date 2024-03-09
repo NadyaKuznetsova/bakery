d2121992c68f320d9d9fa43f9b464ebc68ce90ea
alllexxandra <368970cc@gmail.com>
2024-03-09 13:57:33 +0300

    Создание списка адресов пекарен

--- routes.py
+++ routes.py
@@ -14,10 +14,50 @@ def home():
         year=datetime.now().year
     )
 
+
+bakeries_data = {
+    '1': {
+        'address': 'ул. Коллонтай, 5/1',
+        'working_hours': '09:00-20:00',
+        'nearest_metro': 'Ближайшая станция метро 1'
+    },
+    '2': {
+        'address': 'просп. Елизарова, 3',
+        'working_hours': '8:00-19:00',
+        'nearest_metro': 'Ближайшая станция метро 2'
+    },
+    '3': {
+        'address': 'Софийская ул., 57',
+        'working_hours': '10:00-21:00',
+        'nearest_metro': 'Ближайшая станция метро 3'
+    },
+    '4': {
+        'address': 'ул. Седова, 89, корп. 1',
+        'working_hours': '10:00-21:00',
+        'nearest_metro': 'Ближайшая станция метро 4'
+    },
+    '5': {
+        'address': 'Московский просп., 145А',
+        'working_hours': '10:00-21:00',
+        'nearest_metro': 'Ближайшая станция метро 5'
+    }
+}
+
 @route('/contact')
 @view('contact')
 def contact():
     """Renders the contact page."""
+    bakeries = []
+
+    for bakery_id, data in bakeries_data.items():
+        bakery = {
+            'id': bakery_id,
+            'address': data['address'],
+            'working_hours': data['working_hours'],
+            'nearest_metro': data['nearest_metro']
+        }
+        bakeries.append(bakery)
+
     return dict(
         title='Contact',
         message='Your contact page.',
