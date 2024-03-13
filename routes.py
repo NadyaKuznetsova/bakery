beccd410d72759d292fcd9a1b6dcedc822485059
alllexxandra <368970cc@gmail.com>
2024-03-13 12:01:41 +0300

    Выделение словаря значений в отдельный файл

--- routes.py
+++ routes.py
@@ -14,34 +14,7 @@ def home():
         year=datetime.now().year
     )
 
-
-bakeries_data = {
-    1: {
-        'address': 'ул. Коллонтай, 5/1',
-        'working_hours': '09:00-20:00',
-        'nearest_metro': 'Проспект Большевиков'
-    },
-    2: {
-        'address': 'просп. Елизарова, 3',
-        'working_hours': '8:00-19:00',
-        'nearest_metro': 'Елизаровская'
-    },
-    3: {
-        'address': 'Софийская ул., 57',
-        'working_hours': '10:00-21:00',
-        'nearest_metro': 'Проспект Славы'
-    },
-    4: {
-        'address': 'ул. Седова, 89, корп. 1',
-        'working_hours': '10:00-21:00',
-        'nearest_metro': 'Ломоносовская'
-    },
-    5: {
-        'address': 'Московский просп., 145А',
-        'working_hours': '10:00-21:00',
-        'nearest_metro': 'Электросила'
-    }
-}
+from Addresses import bakeries_data
 
 @route('/contact')
 @view('contact')
